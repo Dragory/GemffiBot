b3ca6a61152ffd5354c75c1093eec6da0f54b0dd
unknown <dragory.up@gmail.com>
2015-08-15 13:19:06 +0300

    fix: don't hang on ctrl+c; throw exceptions after cb
    We now call process.exit() after SIGINT, SIGUSR2, etc.
    We also rethrow uncaught exceptions after running the callback.

--- src/shutdown.js
+++ src/shutdown.js
@@ -6,15 +6,19 @@ export default {
 			if (called) return;
 			called = true;
 			cb(type);
+
+			// Because it's a "once" like binding, we don't have to worry about
+			// infinite recursion here
+			process.exit();
 		}
 
-		process.once('exit', runCb.bind(null, 'exit')); // process.exit()
+		process.once('exit', runCb.bind(null, 'exit', false)); // process.exit()
 		process.once('SIGTERM', runCb.bind(null, 'SIGTERM')); // something
 		process.once('SIGINT', runCb.bind(null, 'SIGINT')); // ctrl+c
 		process.once('SIGUSR2', runCb.bind(null, 'SIGUSR2')); // nodemon
 		process.once('uncaughtException', function(e) {
-			console.log(e);
 			cb();
+			throw e;
 		}); // exceptions
 	}
 };

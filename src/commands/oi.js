4dd9ec5a0b7abe405b62d0096bcf14b7566f94e7
unknown <dragory.up@gmail.com>
2015-08-20 15:38:59 +0300

    feat(oi): add some more OI

--- src/commands/oi.js
+++ src/commands/oi.js
@@ -5,7 +5,7 @@ export default function(message, next) {
 	let idMatch = message.text.match(/^\/oi(?:@${me.username})?\s+(.+)$/);
 	if (idMatch === null) return next();
 
-	let text = idMatch[1].replace(/i/ig, 'oi').replace(/y/ig, 'oy').toUpperCase();
+	let text = idMatch[1].replace(/i/ig, 'oi').replace(/y/ig, 'oy').replace(/[au]/ig, 'o').toUpperCase();
 
 	api.sendMessage(message.chat.id, text);
 };

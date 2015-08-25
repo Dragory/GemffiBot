ffa7ec975879d8ed9b96887a7cf8ba1602f02a39
unknown <dragory.up@gmail.com>
2015-08-25 15:59:46 +0300

    fix(/oi): PROPAH MULTILINE MATCHING

--- src/commands/oi.js
+++ src/commands/oi.js
@@ -2,7 +2,7 @@ import api from '../api';
 import me from '../me';
 
 export default function(message, next) {
-	let idMatch = message.text.match(/^\/oi(?:@${me.username})?\s+(.+)$/m);
+	let idMatch = message.text.match(/^\/oi(?:@${me.username})?\s+((?:.|\s)+)$/m);
 	if (idMatch === null) return next();
 
 	let text = idMatch[1].replace(/i/ig, 'oi').replace(/y/ig, 'oy').replace(/[au]/ig, 'o').toUpperCase();

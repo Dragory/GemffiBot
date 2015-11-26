9a62869b116411d486de88f8574ae7d1d3e0383a
Miikka Virtanen <miikka@mivir.fi>
2015-11-26 16:28:12 +0200

    refactor(markov): raise cand weights to power of 2
    This creates some more rational sentences with small sample sizes (citation needed)

--- src/markov.js
+++ src/markov.js
@@ -74,7 +74,7 @@ function randomKey(obj) {
 }
 
 function weightedRandom(candidates) {
-    let total = Object.keys(candidates).reduce((total, key) => total + candidates[key], 0),
+    let total = Object.keys(candidates).reduce((total, key) => total + Math.pow(candidates[key], 2), 0),
         rand = Math.floor(Math.random() * total);
 
     for (let key in candidates) {
